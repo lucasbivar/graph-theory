9bb94020099698f253dff91090bdd5381413aefe
lucasbivar <lucasbivarfonseca@gmail.com>
2021-08-02 15:19:58 -0300

    feat: implementing guide 3  functions

--- Guide 3/meu_grafo.py
+++ Guide 3/meu_grafo.py
@@ -216,6 +216,49 @@ class MeuGrafo(GrafoListaAdjacencia):
 
         return arvoreBfs
     
+    def __gerar_caminho_ciclo(self, verticeInicial, v, pai, ciclo):
+        '''
+        Gera de maneira recursiva a lista com o ciclo, com base no dicionário 
+        de vértices contendo os seus respectivos pais.
+        :param verticeInicial: Vértice de partida do ciclo
+        :param v: Vertice atual na chamada recursiva
+        :param pai: Dicionário que armazena o vértice que era anterior a um
+        vertice x no fluxo da DFS e a aresta que liga os dois vértices
+        '''
+        if v not in pai: return
+
+        ciclo.append(pai[v][1])
+        ciclo.append(pai[v][0])
+
+        if verticeInicial == pai[v][0]:
+            return
+
+        self.__gerar_caminho_ciclo(verticeInicial, pai[v][0], pai, ciclo)
+
+    def __dfs_ciclo(self, V, arestaAnterior, verticeInicio, verticesAdjacentes, visitados, pai):
+        '''
+        Se for possivel, responsável por encontrar um ciclo saindo do 
+        vértice de partida em um grafo para ele mesmo.
+        :param V: Vértice atual da DFS 
+        :param arestaAnterior: Rótulo da aresta pelo o qual veio o vértice V 
+        no fluxo da DFS
+        :param verticeInicio: Vértice de partida da DFS
+        :param verticesAdjacentes: Dicionário com os vértices adjacentes 
+        de cada vértice do grafo
+        :param visitados: Vértices já visitados na DFS
+        :param pai: Dicionário que armazena o vértice que era anterior a um
+        vertice x no fluxo da DFS e a aresta que liga os dois vértices
+        '''
+        for (verticeAdjacente, rotuloAresta) in verticesAdjacentes[V]:
+            if verticeAdjacente == verticeInicio and rotuloAresta != arestaAnterior:
+                pai[verticeAdjacente] = (V, rotuloAresta)
+                return
+                
+            if verticeAdjacente not in visitados:
+                pai[verticeAdjacente] = (V, rotuloAresta)
+                visitados.add(verticeAdjacente)
+                self.__dfs_ciclo(verticeAdjacente, rotuloAresta, verticeInicio, verticesAdjacentes, visitados, pai)
+
 
     def ha_ciclo(self):
         '''
@@ -223,7 +266,88 @@ class MeuGrafo(GrafoListaAdjacencia):
         :return: Caso exista ciclo, retorna uma lista contendo um ciclo presente 
         no grafo, caso contrário, retorna o valor booleano False
         '''
-        pass
+        verticesAdjacentes = self.__gerarVerticesAdjacencentes()
+
+        # tenta achar um ciclo partindo de cada vértice e voltando pra ele
+        for v in self.N:
+            pai = dict()
+            visitados = set([v])
+            if v not in verticesAdjacentes: continue
+
+            self.__dfs_ciclo(v, None, v, verticesAdjacentes, visitados, pai)
+          
+            ciclo = [v]
+            self.__gerar_caminho_ciclo(v, v, pai, ciclo)
+            if len(ciclo) > 1: return ciclo
+
+        return False
+
+    def __dfs_melhor_proximo_caminho(self, V, distancias, verticesAdjacentes):
+        '''
+        Responsável por realizar a DFS no grafo e armazenar as distâncias de 
+        cada vértice partindo do vértice inicial. As distâncias são armazenadas
+        com objetivo de fazer com que a DFS adaptada que monta o caminho,
+        encontre o melhor próximo vértice pra ir, ou seja, o vértice que vai 
+        garantir uma maior profundidade
+        :param V: Vértice atual da DFS
+        :param distancias: Dicionário que inicia apenas com o vértice de partida
+        e vai sendo preenchido com as distâncias dos vértices do grafo para o vértice 
+        de partida ao longo da DFS
+        :param verticesAdjacentes: Dicionário com os vértices adjacentes 
+        de cada vértice do grafo
+        '''
+        if V not in verticesAdjacentes: return
+
+        for (verticeAdjacente, rotuloAresta) in verticesAdjacentes[V]:
+            if verticeAdjacente not in distancias:
+                distancias[verticeAdjacente] = distancias[V] + 1
+                self.__dfs_melhor_proximo_caminho(verticeAdjacente, distancias, verticesAdjacentes)
+    
+    def __dfs_caminho(self, V, visitados, caminho, verticesAdjacentes):
+        '''
+        Responsável por montar e encontrar o maior caminho partindo do 
+        vértice V em um grafo.
+        :param V: Vértice atual da DFS 
+        :param visitados: Conjunto contendo os vértices já visitados pela DFS adaptada
+        :param caminho: Lista para armazenar o caminho realizado pela DFS adaptada
+        :param verticesAdjacentes: Dicionário com os vértices adjacentes 
+        de cada vértice do grafo
+        '''
+        maiorCaminho = -1
+        melhorVertice = None
+        arestaMelhorVertice = None
+
+        # novo grafo sem os vértices visitados
+        novoGrafo = deepcopy(self)
+        for v in visitados:
+            if novoGrafo.existeVertice(v):
+                novoGrafo.removeVertice(v)
+        
+        verticesAdjNovoGrafo = novoGrafo.__gerarVerticesAdjacencentes()
+
+        # escolhe o melhor próximo vértice (que vai garantir um maior caminho)
+        for (verticeAdjacente, rotuloAresta) in verticesAdjacentes[V]:
+            if verticeAdjacente not in visitados:
+                distancias = {verticeAdjacente: 0}
+                novoGrafo.__dfs_melhor_proximo_caminho(verticeAdjacente, distancias, verticesAdjNovoGrafo)
+                
+                distanciaMaxima = -1
+                for key, value in distancias.items():
+                    if value > distanciaMaxima: distanciaMaxima = value
+
+                if distanciaMaxima > maiorCaminho:
+                    maiorCaminho = distanciaMaxima
+                    melhorVertice = verticeAdjacente
+                    arestaMelhorVertice = rotuloAresta
+
+        # adiciona o melhor vértice no caminho, e chama a DFS para ele
+        if melhorVertice != None:
+            visitados.add(melhorVertice)
+
+            caminho.append(arestaMelhorVertice)
+            caminho.append(melhorVertice)
+
+            self.__dfs_caminho(melhorVertice, visitados, caminho, verticesAdjacentes)
                
 
     def caminho(self, n):
@@ -233,11 +357,36 @@ class MeuGrafo(GrafoListaAdjacencia):
         :return: Uma lista contendo o caminho de tamanho "n" caso exista, caso 
         contrário retorna o valor booleano False
         '''
-        pass
+        if n <= 0: return False
+
+        verticesAdjacentes = self.__gerarVerticesAdjacencentes()
+
+        # para cada vértice do grafo, encontra o maior caminho partindo dele,
+        # enquanto não achar um que satisfaz o caminho de tamanho "n"
+        for v in self.N:
+            if v not in verticesAdjacentes: continue
+
+            visitados = set([v])
+            caminho = [v]
+            self.__dfs_caminho(v, visitados, caminho, verticesAdjacentes)
+
+            if n < len(visitados):
+                return caminho[:2*n+1]
+
+        return False
 
     def conexo(self):
         '''
         Verifica se o grafo é conexo
         :return: Um valor booleano que indica se o grafo é ou não conexo
         '''
-        pass
+        qtdVertices = len(self.N)
+        
+        # chama a BFS para um vértice qualquer
+        arvoreBFS = self.bfs(self.N[0])
+
+        qtdVerticesCompConexa = len(arvoreBFS.N)
+
+        # verifica se a quantidade de vértices da componente conexa gerada pela BFS
+        # é igual a quantidade de vértices do grafo original
+        return qtdVertices == qtdVerticesCompConexa
